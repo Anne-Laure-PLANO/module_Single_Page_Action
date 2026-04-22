381dd9f4eae09ca1f05d48115bfd4cd31290d5ca
Anne-Laure-PLANO <plano-devillard@orange.fr>
2026-04-22 10:48:01 +0200

    Réalisation de l'exercice 2 : transformation des fonctions et méthodes en fonctions flêchées.

--- 2-functions/script.js
+++ 2-functions/script.js
@@ -1,19 +1,27 @@
 //2.1 Fonctions simples
-const sayHello = function() {
+const sayHello = () => {
     console.log('Hello')
 };
 
-const sayMyName = function(first, last) {
+const sayMyName = (first, last) => {
     console.log(first, last)
 };
 
-const sayMyAge = function(age) {
+const sayMyAge = (age) => {
     console.log('You are ' + age + ' years old')
 };
 
 sayHello();
 sayMyName('Odile', 'Crok');
 sayMyAge(23);
+
+/* -------Exercices ------------
+Section 1
+Convertissez les 3 fonctions avec la notation "Arrow function" et vérifiez que le code fonctionne comme avant la conversion.
+
+*/
+
+
 
 //----------------------//
 //2.2 this
@@ -22,12 +30,29 @@ const object = {
     color: 'red',
     shape: 'circle',
     threeDimensions: false,
-    showThis: function() {
+    showThis(){
         console.log(this)
     }
 };
-
+// showthis est une méthode relative à l'objet dont elle dépend.
 object.showThis();
+
+
+/* -------Exercices ------------
+Section 2
+Avant toute modification : assurez-vous de comprendre le code déjà écrit ! Au besoin commentez-le.
+Convertissez la fonction showThis en notation "Arrow function".
+Observez le résultat. Que pouvez-vous en conclure ?
+    La fonction d'une méthode ne peut pas s'écrire de manière flêchée comme généralement, car dans ce cas elle pointe vers le contexte global.
+    deux façons de les écrire dans un objet : 
+        titreMethode : function(){        } 
+        ou :
+        titreMethode(){}
+
+*/
+
+
+
 
 //----------------------//
 //2.3 Application
@@ -37,12 +62,30 @@ const odile = {
         first: 'Odile',
         last: 'Crok'
     },
-    age: 23,
-    //sayHello: ...
-    //sayMyName:...
-    //sayMyAge:...
+    age: 25,
+    sayHello : () =>{
+        console.log('Hello')
+    },
+    sayMyName () {
+        console.log(this.name.first, this.name.last)
+    },
+
+    sayMyAge() {
+        console.log('You are ' + this.age + ' years old')
+    }
+
 };
 
-//odile.sayHello()
-//odile.sayMyName()
-//odile.sayMyAge()
+odile.sayHello()
+odile.sayMyName()
+odile.sayMyAge()
+
+
+/* -------Exercices ------------
+Section 3
+Complétez le code pour reproduire le comportement de la section 1 mais cette fois-ci grâce à l'objet odile. 
+Vous utiliserez dès que possible des "Arrow functions".
+    Les arrow functions ne peuvent pas s'utiliser dans un objet lors de l'appel de "this".
+
+
+*/
